d64750fbf7ab35680e5bc8acef8051cbaade1577
root <root@DESKTOP-TER5CPC>
2024-10-27 05:28:55 +0100

    scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse){
   const task2 = true;
 
   if (trueOrFalse){
-    let task = true;//let limits this o the if block scope
-    let task = false;// let prevents overwrioting the outer const
+    const task = true;//let limits this o the if block scope
+    const task = false;// let prevents overwrioting the outer const
   }
 
   return[task, task2]
