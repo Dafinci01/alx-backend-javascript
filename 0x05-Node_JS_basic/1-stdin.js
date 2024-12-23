3fcfb8ff08c3f018e05a2597d1b8818179dec841
Dafinci01 <Davehacker382gmail.com>
2024-12-23 08:21:56 +0100

    process stand in

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
 // using node js for input and output via commandlie
 
 
-process.stdin.on('readable', () => {
+process.stdin.on('readable',() => {
   const name = process.stdin.read();
 
 
-  if (name && name.trim()) {
-    process.stdout.write(`Your name is: ${name.trim()}\n`);
-    else {
-      process.stdout.write("Name cannot be empty. Please try again: \n');
-    }
-  });
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
 
