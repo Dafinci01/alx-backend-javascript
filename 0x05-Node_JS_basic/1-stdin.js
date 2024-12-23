14515b2a8b6200567dca20feb9e5235488073797
Dafinci01 <Davehacker382gmail.com>
2024-12-23 08:54:50 +0100

    process stnd in

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
-// using node js for input and output via commandlie
-//
-process.stdin.on('readable',() => {
-  const chunk  = process.stdin.read();
+//proces stdout.write to display to the screen
+process.stdout.write('Welcome to Alx, what is your name?\n');
+//process stdin.on to read and receive input
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-
-  if(chunk) {
+  if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
