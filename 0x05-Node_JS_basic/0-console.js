166d1b17ffa239a1ef6d6058ef3bb0f338e5d365
Dafinci01 <Davehacker382gmail.com>
2024-12-26 23:53:50 +0100

    correction on sync and async way of parsing file system on node

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,21 @@
-/**
- * Displays a message on the STDOUT.
- * @param {String} msg The message to display.
- * @author david odelana
- */
-const displayMessage = (msg) => {
-  console.log(msg);
-};
+//In the file 0-console.js, create a function named displayMessage that prints in STDOUT the string argumet.
 
-module.exports = displayMessage;
+
+
+const http = require('http');//importing the http module
+
+const hostname = '127.0.01'//host ip
+const port = 3000;// port numer for servwer
+
+//creating the server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; // http stastus code means ok
+    res.setHeader('Content-Type', 'text/plain'); //setting response heasder to text
+    res.end('displayMesssage\n');//sending the response
+});
+
+
+//startting the server
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
